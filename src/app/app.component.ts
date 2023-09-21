25d778d8d5dc2fb42c4ff93df574db16152b7efc
joster-dev <joster@knights.ucf.edu>
2023-09-21 18:38:12 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rttl-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'rttl';
+}
