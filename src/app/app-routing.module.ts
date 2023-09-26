89bc168288db72d6ecaa7800aa76a11af48f955e
joster-dev <joster@knights.ucf.edu>
2023-09-25 22:40:21 -0400

    feat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { OzarksComponent } from './ozarks/ozarks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'ozarks', component: OzarksComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/ozarks/ozarks.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rttl-ozarks',
+  templateUrl: './ozarks.component.html',
+  styleUrls: ['./ozarks.component.scss']
+})
+export class OzarksComponent {
+
+}
