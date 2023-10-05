d3c7a0a8cbf8ab01b6b03cb70f16993672aa89d7
joster-dev <joster@knights.ucf.edu>
2023-10-04 21:44:30 -0400

    feat

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'rttl-home',
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  images = [
+    'assets/photo_2023-09-17_14-00-32.jpg',
+    'assets/photo_2023-09-18_23-06-17.jpg'
+  ]
+  imageIndex = 0;
 
+  scroll(id: string) {
+    const el = document.getElementById(id);
+    if (el) {
+      el.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
