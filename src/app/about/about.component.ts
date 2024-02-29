03b9e28a4ea2f7d852cdf8a1de3759067b2761e2
joster-dev <joster@knights.ucf.edu>
2024-02-28 21:08:03 -0500

    feat part 1

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rttl-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { DonateComponent } from './donate/donate.component';
 import { FooterComponent } from './footer/footer.component';
 import { AprilEventComponent } from './april-event/april-event.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AprilEventComponent } from './april-event/april-event.component';
     HomeComponent,
     DonateComponent,
     FooterComponent,
-    AprilEventComponent
+    AprilEventComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
